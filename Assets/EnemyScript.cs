b53385f1ad289ba0b76b0189429803550880f11c
shotaro-ooe <shoooe97@gmail.com>
2018-11-27 16:24:43 +0900

    Sec

---
+++ Assets/EnemyScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyScript : MonoBehaviour
+{
+
+    float speed = 8.0f;
+
+    // Use this for initialization
+    void Start()
+    {
+        Destroy(this.gameObject, 5.0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // this.transform.position += new Vector3(0, 0, speed * Time.deltaTime);
+        this.transform.position += this.transform.forward * speed * Time.deltaTime;
+    }
+}
---
+++ Assets/EnemySpawnerScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawnerScript : MonoBehaviour
+{
+
+    public GameObject enemy;
+
+    float timer;
+
+    // Use this for initialization
+    void Start()
+    {
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if (timer > 1)
+        {
+            Instantiate(enemy, this.transform.position + this.transform.right * Random.Range(-10.0f, 10.0f), this.transform.rotation);
+            timer = 0;
+        }
+    }
+}
